570db0bea2719b98655386d3274feb656f2b8108
Marc <marc@beatentrack.games>
2024-08-29 13:47:37 +0200

    refs #4331 - Update Startanimation

--- Assets/_scripts/kielRegion/StartingAnimation.cs
+++ Assets/_scripts/kielRegion/StartingAnimation.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartingAnimation : MonoBehaviour
 {
     [SerializeField] private CanvasRenderer[] m_Sprites;
+    [SerializeField] private CanvasRenderer m_Logo;
+    [SerializeField] private Image m_LogoBg;
     [SerializeField] private float m_AnimSpeed = 1f;
     [SerializeField] private float m_Delay = 0.2f;
 
     private void Start()
     {
-        StartAnimation();
+        //StartAnimation();
+        StartLogoAnimation();
     }
 
     private void StartAnimation()
@@ -29,5 +33,20 @@ public class StartingAnimation : MonoBehaviour
             sequence.Append(sprite.transform.DOMove(endPos, m_AnimSpeed).SetEase(Ease.InOutSine));
             sequence.Play();
         }
+    }
+
+    private void StartLogoAnimation()
+    {
+        m_Logo.transform.position = new Vector3(Screen.width / 2, Screen.height / 2, m_Logo.transform.position.z);
+        m_Logo.transform.localScale = Vector3.zero;
+        
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(m_Logo.transform.DOScale(Vector3.one, m_AnimSpeed).SetEase(Ease.OutBack));
+        sequence.AppendInterval(1f);
+        sequence.Append(m_LogoBg.DOFade(0, m_AnimSpeed));
+        sequence.Join(m_Logo.transform.DOScale(new Vector3(0.3f, 0.3f, 1), m_AnimSpeed));
+        sequence.Join(m_Logo.transform.DOMove(new Vector3(3400, 222, m_Logo.transform.position.z), m_AnimSpeed));
+        sequence.AppendCallback(()=> m_LogoBg.gameObject.SetActive(false));
+        sequence.Play();
     }
 }
